3eb2b8c4d85d3685d33a0f79b32a0304b251e32c
Jun-Hong-Xu <junhong.xu@ucdconnect.ie>
2020-02-20 09:39:35 +0000

    update scheduler

--- src/spiderman/crawler_scheduler.py
+++ src/spiderman/crawler_scheduler.py
@@ -1,16 +1,17 @@
 import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BlockingScheduler
 import crawler_insert_table
 
 
 def timed_task():
-    # crawler_insert_table.main()
+    crawler_insert_table.main()
     f = open("crawler_log.txt", 'a')
     f.write(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")[:-7])
     f.write("\n")
     f.write("Table station has updated with: %i rows. \n" %crawler_insert_table.station_renew_row)
     f.write("Table station_weather has updated with: %i rows. \n" %crawler_insert_table.station_weather_renew_row)
     f.close()
+    print("Test info: output at", datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")[:-7])
 
 
 if __name__ == '__main__':
@@ -19,11 +20,11 @@ if __name__ == '__main__':
     end = input("Enter the end time: ")
 
     # Create the instance of schedulers
-    scheduler = BackgroundScheduler()
+    scheduler = BlockingScheduler()
 
     # Set the trigger to interval, and interval is 300 second (5 minutes)
     # start_date = "2020-2-19 20:43:20"
-    scheduler.add_job(timed_task, 'interval', seconds=1, start_date=start, end_date=end)
+    scheduler.add_job(timed_task, 'interval', seconds=300, start_date=start, end_date=end)
 
     # Start the scheduler
     scheduler.start()
